4452f46ab53d5011bc9843e6cca74f93bfecf1df
SanderDeKeukelaere <dekeukelaeresander@gmail.com>
2023-12-24 14:16:52 +0100

    Changed scene setup
    Changed quads per meter for the terrain from 4 to 1
    Added a plane to the scene that represents sea level

--- src/Erosion/Scenes/Sample.cpp
+++ src/Erosion/Scenes/Sample.cpp
@@ -11,6 +11,11 @@
 #include <Components/RenderComponents/TerrainComponent.h>
 #include "../Components/TerrainGeneratorComponent.h"
 
+#include <Graphics/Mesh.h>
+#include <Graphics/Material.h>
+#include <Shaders/Pos3D.h>
+#include <Components/RenderComponents/MeshRenderer.h>
+
 void Erosion::SampleScene::Load(leap::Scene& scene)
 {
 	const auto pCamera{ scene.CreateGameObject("Main camera") };
@@ -24,6 +29,16 @@ void Erosion::SampleScene::Load(leap::Scene& scene)
 	pDirLight->AddComponent<leap::DirectionalLightComponent>()->GetTransform()->SetWorldRotation(60, 45, 0, true);
 
 	const auto pTerrain{ scene.CreateGameObject("Terrain") };
-	pTerrain->AddComponent<leap::TerrainComponent>()->SetSize(256, 4);
+	pTerrain->AddComponent<leap::TerrainComponent>()->SetSize(256, 1);
 	pTerrain->AddComponent<Erosion::TerrainGeneratorComponent>();
+
+	const auto pPlane{ scene.CreateGameObject("Plane") };
+	const leap::Mesh planeMesh{ "Data/Engine/Models/Plane.obj" };
+	const leap::Material planeMat{ leap::Material::Create<leap::graphics::shaders::Pos3D>("Plane") };
+	planeMat.Set("gColor", glm::vec4{ 0.375f, 0.5468f, 0.6992f, 1.0f });
+	const auto pPlaneRenderer{ pPlane->AddComponent<leap::MeshRenderer>() };
+	pPlaneRenderer->SetMesh(planeMesh);
+	pPlaneRenderer->SetMaterial(planeMat);
+	pPlane->GetTransform()->Scale(1024.0f);
+	pPlane->GetTransform()->Translate(512.0f, 50.0f, 512.0f);
 }
